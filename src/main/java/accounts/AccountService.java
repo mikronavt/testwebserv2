5f4352f7f71a18b9eb9fea23240899948e54329a
Tsaplin Anatoliy <chgb-tol@ya.ru>
2015-12-24 18:26:32 +0300

    done

---
+++ src/main/java/accounts/AccountService.java
@@ -0,0 +1,39 @@
+package accounts;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by User on 23.12.2015.
+ */
+public class AccountService {
+    private final Map<String, UserProfile> loginToProfile;
+    private final Map<String, UserProfile> sessionIdToProfile;
+
+    public AccountService(){
+        loginToProfile = new HashMap<String, UserProfile>();
+        sessionIdToProfile = new HashMap<String, UserProfile>();
+    }
+
+    public void addNewUser(UserProfile userProfile){
+        loginToProfile.put(userProfile.getLogin(), userProfile);
+
+    }
+
+    public UserProfile getUserByLogin(String login){
+        return loginToProfile.get(login);
+    }
+
+    public UserProfile getUserBySessionId(String sessionId){
+        return sessionIdToProfile.get(sessionId);
+    }
+
+    public void addSession(String sessionId, UserProfile userProfile){
+        sessionIdToProfile.put(sessionId, userProfile);
+    }
+
+    public void deleteSession(String sessionId){
+        sessionIdToProfile.remove(sessionId);
+    }
+
+}
